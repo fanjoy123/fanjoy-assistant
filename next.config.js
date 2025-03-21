62a5d9b4ed7b9a1f28752fcb60fce266b474cd9d
Chris Vaccarino <chrisvaccarino@gmail.com>
2025-03-21 15:29:26 -0400

    Add Vercel configuration

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['localhost'],
+    unoptimized: true
+  },
+}
+
+module.exports = nextConfig
